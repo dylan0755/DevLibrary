4de040035277d3ef7d8761ee2361fa3a7f1bce07
bingdao_1994@163.com <bingdao_1994@163.com>
2016-12-25 23:32:11 +0800

    完善EditNumberHelper

--- app/src/main/java/com/dankal/mylibrary/ui/edittext/EditNumberActivity.java
+++ app/src/main/java/com/dankal/mylibrary/ui/edittext/EditNumberActivity.java
@@ -23,46 +23,44 @@ public class EditNumberActivity extends Activity {
     EditText edtDuration;
     @Bind(R.id.tv_pay)
     TextView tv_pay;
-    private int priceUnit=50;
+    private int priceUnit = 50;
 
 
     private EditNumberHelper mHelper;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_editnumber);
         ButterKnife.bind(this);
-        mHelper=new EditNumberHelper(1,100,1);
+        mHelper = new EditNumberHelper(this, 1, 999, 1);
         mHelper.bindEditText(edtDuration);
-        mHelper.setTextChangedListener(new EditNumberHelper.TextChangedListener() {
+        mHelper.setMaxValueTip("最大不能超过999");
+        mHelper.setInputListener(new EditNumberHelper.InputChangedListener() {
             @Override
-            public void onTextChanged(int numStr) {
+            public void onNumberChanged(int numStr) {
                 totalMoney(numStr);
+
             }
         });
     }
 
     @OnClick({R.id.iv_reduce, R.id.iv_add})
     public void onClick(View view) {
-        int num=0;
+        int num = 0;
         switch (view.getId()) {
             case R.id.iv_reduce:
-                num=mHelper.reduce();
-                String reduce=Integer.toString(num);
-                edtDuration.setText(reduce);
-                totalMoney(num);
+                mHelper.reduce();
                 break;
             case R.id.iv_add:
-                num=mHelper.increase();
-                String reduce2=Integer.toString(num);
-                edtDuration.setText(reduce2);
-                totalMoney(num);
+                mHelper.increase();
                 break;
         }
     }
 
-    private void totalMoney(int dur){
-        int pay=dur*priceUnit;
-        tv_pay.setText(Integer.toString(pay)+"元");
+    private void totalMoney(int dur) {
+        int pay = dur * priceUnit;
+        tv_pay.setText(Integer.toString(pay) + "元");
     }
+
 }
---
+++ dylanlib/src/main/java/com/dylan/library/util/LoginRegUtil.java
@@ -0,0 +1,46 @@
+package com.dylan.library.util;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+/**
+ * Created by Dylan on 2016/12/22.
+ */
+
+public class LoginRegUtil {
+
+
+
+    public static boolean phonePassCheck(String phone, String pass){
+          if (!checkPhone(phone)){
+              return false;
+          }
+
+        if (!StringCheck.isValid(pass)) {
+            ToastUtil.toToast("请输入密码");
+            return false;
+        }
+
+        return true;
+    }
+
+    private static boolean checkPhone(String phone) {
+        if (phone == null || phone.isEmpty()) {
+            ToastUtil.toToast("请输入手机号");
+            return false;
+        }
+        String reg = "^1(3[0-9]|4[57]|5[0-35-9]|7[0135678]|8[0-9])\\d{8}$";
+        Pattern pattern = Pattern.compile(reg);
+        Matcher matcher = pattern.matcher(phone);
+        boolean flag = matcher.matches();
+        if (flag) {//验证通过
+            return true;
+        } else {
+            ToastUtil.toToast("无效手机号");
+            return false;
+        }
+    }
+
+
+
+}
