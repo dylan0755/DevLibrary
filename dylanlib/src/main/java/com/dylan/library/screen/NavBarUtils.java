691b88444e9a17817ca695493fc54c01792a5e50
bingdao_1994@163.com <bingdao_1994@163.com>
2017-11-15 00:15:02 +0800

    增加NavBarUtils  判断是否有包含底部虚拟按键（如华为手机就有），隐藏和显示方法。

---
+++ dylanlib/src/main/java/com/dylan/library/screen/NavBarUtils.java
@@ -0,0 +1,100 @@
+package com.dylan.library.screen;
+
+import android.app.Activity;
+import android.content.Context;
+import android.content.res.Resources;
+import android.os.Build;
+import android.view.View;
+import android.view.ViewConfiguration;
+
+import java.lang.reflect.Method;
+
+/**
+ * Created by Dylan on 2017/11/13.
+ */
+
+public class NavBarUtils {
+
+    /**
+     * 检查是否存在虚拟按键栏
+     * @param context
+     * @return
+     */
+    public static boolean hasNavBar(Context context) {
+        try{
+            Resources res = context.getResources();
+            int resourceId = res.getIdentifier("config_showNavigationBar", "bool", "android");
+            if (resourceId != 0) {
+                boolean hasNav = res.getBoolean(resourceId);
+                // check override flag
+                String sNavBarOverride = getNavBarOverride();
+                if ("1".equals(sNavBarOverride)) {
+                    hasNav = false;
+                } else if ("0".equals(sNavBarOverride)) {
+                    hasNav = true;
+                }
+                return hasNav;
+            } else { // fallback
+                return !ViewConfiguration.get(context).hasPermanentMenuKey();
+            }
+        }catch (Exception e){
+
+        }
+
+        return false;
+
+    }
+
+    /**
+     * 判断虚拟按键栏是否重写
+     * @return
+     */
+    private static String getNavBarOverride() {
+        String sNavBarOverride = null;
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+            try {
+                Class c = Class.forName("android.os.SystemProperties");
+                Method m = c.getDeclaredMethod("get", String.class);
+                m.setAccessible(true);
+                sNavBarOverride = (String) m.invoke(null, "qemu.hw.mainkeys");
+            } catch (Throwable e) {
+            }
+        }
+        return sNavBarOverride;
+    }
+    /**
+     * 隐藏虚拟按键，并且全屏
+     */
+    public static void hideNavBar(Activity activity) {
+        if (activity==null)return;
+        //隐藏虚拟按键，并且全屏
+        if (Build.VERSION.SDK_INT > 11 && Build.VERSION.SDK_INT < 19) {
+            View v = activity.getWindow().getDecorView();
+            v.setSystemUiVisibility(View.GONE);
+        } else if (Build.VERSION.SDK_INT >= 19) {
+            View decorView = activity.getWindow().getDecorView();
+            int uiOptions = View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                    | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY | View.SYSTEM_UI_FLAG_FULLSCREEN;
+            decorView.setSystemUiVisibility(uiOptions);
+        }
+    }
+
+
+    public static void showNavBar(Activity activity, boolean statusLightMode){
+        if (activity==null)return;
+        if (Build.VERSION.SDK_INT > 11 && Build.VERSION.SDK_INT < 19) {
+            View v = activity.getWindow().getDecorView();
+            v.setSystemUiVisibility(View.VISIBLE);
+        } else if (Build.VERSION.SDK_INT >= 19) {
+            View decorView = activity.getWindow().getDecorView();
+            if (!statusLightMode){
+                int uiOptions = View.SYSTEM_UI_FLAG_VISIBLE;
+                decorView.setSystemUiVisibility(uiOptions);
+            }else{
+                int uiOptions = View.SYSTEM_UI_FLAG_VISIBLE| View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR;
+                decorView.setSystemUiVisibility(uiOptions);
+            }
+
+        }
+    }
+}
