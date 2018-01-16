99df1903be59f3bb86b1807dcd90826ecafdf358
Dylan <Dylan0759@163.com>
2018-01-16 09:28:33 +0800

    增加友盟渠道获取

--- dylanlib/src/main/java/com/dylan/library/utils/ChannelUtils.java
+++ dylanlib/src/main/java/com/dylan/library/utils/ChannelUtils.java
@@ -1,4 +1,4 @@
-package com.wxhkj.weixiuhui.util;
+package com.dylan.library.utils;
 
 import android.content.Context;
 import android.content.pm.ApplicationInfo;
@@ -9,28 +9,33 @@ import android.content.pm.PackageManager;
  */
 
 public class ChannelUtils {
-        public static String getChannelName(Context context) {
-            if (context == null) {
-                return null;
-            }
-            String channelName = null;
-            try {
-                PackageManager packageManager = context.getPackageManager();
-                if (packageManager != null) {
-                    //注意此处为ApplicationInfo，因为友盟设置的meta-data是在application标签中
-                    ApplicationInfo applicationInfo = packageManager.
-                            getApplicationInfo(context.getPackageName(), PackageManager.GET_META_DATA);
-                    if (applicationInfo != null) {
-                        if (applicationInfo.metaData != null) {
-                            //这里的UMENG_CHANNEL要与manifest中的配置文件标识一致
-                            channelName = String.valueOf(applicationInfo.metaData.get("UMENG_CHANNEL"));
-                        }
+    /**
+     * 获取当前是那个渠道，用友盟的统计方法
+     * @param context
+     * @return
+     */
+    public static String getUmengChannel(Context context) {
+        if (context == null) {
+            return null;
+        }
+        String channelName = null;
+        try {
+            PackageManager packageManager = context.getPackageManager();
+            if (packageManager != null) {
+                //注意此处为ApplicationInfo，因为友盟设置的meta-data是在application标签中
+                ApplicationInfo applicationInfo = packageManager.
+                        getApplicationInfo(context.getPackageName(), PackageManager.GET_META_DATA);
+                if (applicationInfo != null) {
+                    if (applicationInfo.metaData != null) {
+                        //这里的UMENG_CHANNEL要与manifest中的配置文件标识一致
+                        channelName = String.valueOf(applicationInfo.metaData.get("UMENG_CHANNEL"));
                     }
-
                 }
-            } catch (PackageManager.NameNotFoundException e) {
-                e.printStackTrace();
+
             }
-            return channelName;
+        } catch (PackageManager.NameNotFoundException e) {
+            e.printStackTrace();
+        }
+        return channelName;
     }
 }
