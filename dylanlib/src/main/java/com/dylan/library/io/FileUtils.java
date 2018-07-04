6a70ac52f5784b9017da0b9fb83668fef2db39bc
Dylan <dylan0759@163.com>
2018-07-04 09:49:11 +0800

    FileUtils 添加 压缩到指定目录的方法

--- dylanlib/src/main/java/com/dylan/library/io/FileUtils.java
+++ dylanlib/src/main/java/com/dylan/library/io/FileUtils.java
@@ -3,10 +3,15 @@ package com.dylan.library.io;
 import android.content.Context;
 
 import com.dylan.library.device.SDCardUtils;
+import com.dylan.library.utils.Logger;
 
 import java.io.File;
 import java.io.FileOutputStream;
+import java.io.IOException;
 import java.io.InputStream;
+import java.util.Enumeration;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipFile;
 
 /**
  * Created by Dylan on 2016/12/31.
@@ -66,7 +71,35 @@ public class FileUtils {
     }
 
 
+    public static void unzip(String srcZipPath,String dirName) throws IOException {
+        ZipFile zipFile=new ZipFile(srcZipPath);
+        for (Enumeration<? extends ZipEntry> entries = zipFile.entries(); entries.hasMoreElements();) {
+            ZipEntry entry = entries.nextElement();
+            String zipEntryName = entry.getName();
+            InputStream in = zipFile.getInputStream(entry);
+            // String outPath = (desDir + name +"/"+ zipEntryName).replaceAll("\\*", "/");
+            String outPath = (dirName+"/"+ zipEntryName).replaceAll("\\*", "/");
+            // 判断路径是否存在,不存在则创建文件路径
+            File file = new File(outPath.substring(0, outPath.lastIndexOf('/')));
+            if (!file.exists()) {
+                file.mkdirs();
+            }
+            if (new File(outPath).isDirectory()) {
+                continue;
+            }
+            FileOutputStream out = new FileOutputStream(outPath);
+            byte[] buf1 = new byte[1024];
+            int len;
+            while ((len = in.read(buf1)) > 0) {
+                out.write(buf1, 0, len);
+            }
+            in.close();
+            out.close();
+        }
+        Logger.e("解压完毕.......");
+        return;
 
+    }
 
 
 
