4d4b47bbd63146a431b4007d41bfde332ca8a59a
Dylan <dylan0759@163.com>
2019-08-14 11:58:38 +0800

    更新ClipBoardUtils.java

--- dylanlib/src/main/java/com/dylan/library/utils/ClipBoardUtils.java
+++ dylanlib/src/main/java/com/dylan/library/utils/ClipBoardUtils.java
@@ -11,12 +11,23 @@ import android.widget.Toast;
 
 public class ClipBoardUtils {
 
-    public void copy(Context context,String text){
+
+
+    public static void copy(Context context,String text){
         if (context==null||(text==null||text.isEmpty()))return;
         ClipboardManager myClipboard = (ClipboardManager)context.getSystemService(Context.CLIPBOARD_SERVICE);
         ClipData myClip = ClipData.newPlainText("text", text);
         myClipboard.setPrimaryClip(myClip);
-        Toast.makeText(context,"已复制至粘贴板",Toast.LENGTH_SHORT).show();
+        Toast.makeText(context,"复制成功",Toast.LENGTH_SHORT).show();
+    }
+
+
+    public static void copy(Context context,String text,String tip){
+        if (context==null||(text==null||text.isEmpty()))return;
+        ClipboardManager myClipboard = (ClipboardManager)context.getSystemService(Context.CLIPBOARD_SERVICE);
+        ClipData myClip = ClipData.newPlainText("text", text);
+        myClipboard.setPrimaryClip(myClip);
+        Toast.makeText(context,""+tip,Toast.LENGTH_SHORT).show();
     }
 
 }
