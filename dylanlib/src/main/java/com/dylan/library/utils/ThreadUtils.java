d9fb43cd6e04875e925276f2a4273b5320ef2d1c
Dylan <dylan0759@163.com>
2019-08-15 13:00:32 +0800

    ThradUtils更新

--- dylanlib/src/main/java/com/dylan/library/utils/ThreadUtils.java
+++ dylanlib/src/main/java/com/dylan/library/utils/ThreadUtils.java
@@ -8,4 +8,11 @@ public class ThreadUtils {
 		boolean flag=Looper.getMainLooper().getThread()==Thread.currentThread()?true:false;
 		return flag;
 	}
+
+	public static void startNewThread(Runnable runnable){
+		if (runnable==null)return;
+		new Thread(runnable).start();
+	}
+
+
 }
