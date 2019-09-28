ad19950fa80dbbd728e6a078bad0a1bb1c88ce4a
Dylan <dylan0759@163.com>
2019-09-28 10:43:00 +0800

    RedPointTextView  增加修改红点颜色

--- app/src/main/java/com/dylan/mylibrary/ui/RedPointTextViewActivity.java
+++ app/src/main/java/com/dylan/mylibrary/ui/RedPointTextViewActivity.java
@@ -1,6 +1,7 @@
 package com.dylan.mylibrary.ui;
 
 import android.app.Activity;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 
@@ -16,13 +17,17 @@ public class RedPointTextViewActivity extends Activity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_redpointtextview);
-        RedPointTextView textView1= (RedPointTextView) findViewById(R.id.redText1);
-        RedPointTextView textView2= (RedPointTextView) findViewById(R.id.redText2);
+        RedPointTextView textView1=findViewById(R.id.redText1);
+        RedPointTextView textView2=  findViewById(R.id.redText2);
+        RedPointTextView textView3=findViewById(R.id.redText3);
         textView1.setText("天安门广场上鸣礼炮21响。");
-        textView1.setPointVisible();
+        textView1.showPoint();
         textView2.setText("国家主席习近平3日下午在人民大会堂北大厅举行仪式，欢迎津巴布韦总统埃默森·姆南加古瓦对我国进行国事访问。");
-        textView2.setPointVisible();
+        textView2.showPoint();
 
+
+        textView3.setText("国家主席习近平3日下午在人民大会堂北大厅举行仪式，欢迎津巴布韦总统埃默森·姆南加古瓦对我国进行国事访问。");
+        textView3.showPoint(Color.DKGRAY);
     }
 
 
--- dylanlib/src/main/java/com/dylan/library/widget/RedPointTextView.java
+++ dylanlib/src/main/java/com/dylan/library/widget/RedPointTextView.java
@@ -5,88 +5,102 @@ import android.content.res.TypedArray;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
+import android.support.v7.widget.AppCompatTextView;
 import android.util.AttributeSet;
-import android.widget.TextView;
 
 import com.dylan.library.R;
 
 
 /**
- *  文本末尾追加小圆点
+ * 文本末尾追加小圆点
  */
-public class RedPointTextView extends TextView {
+public class RedPointTextView extends AppCompatTextView {
     private int visibility = 0;
     private float pointSize;
-
+    private int pointColor = Color.RED;
+    Paint mPaint ;
     public RedPointTextView(Context context) {
         super(context);
-        getAttribute(context,null);
+        getAttribute(context, null);
     }
 
     public RedPointTextView(Context context, AttributeSet attrs) {
         super(context, attrs);
-        getAttribute(context,attrs);
+        getAttribute(context, attrs);
 
     }
 
     public RedPointTextView(Context context, AttributeSet attrs, int defStyle) {
         super(context, attrs, defStyle);
-        getAttribute(context,attrs);
+        getAttribute(context, attrs);
     }
 
     public void getAttribute(Context context, AttributeSet attrs) {
-        if(attrs != null) {
+        if (attrs != null) {
             TypedArray array = getContext().obtainStyledAttributes(attrs, R.styleable.RedPointTextView);
-            pointSize =array.getDimension(R.styleable.RedPointTextView_redPointSize,8);
+            pointSize = array.getDimension(R.styleable.RedPointTextView_redPointSize, 8);
+            pointColor = array.getColor(R.styleable.RedPointTextView_pointColor, Color.RED);
             array.recycle();
         }
+        mPaint= new Paint();
+        mPaint.setColor(pointColor);
+        mPaint.setAntiAlias(true);
+        mPaint.setDither(true);
+        mPaint.setStyle(Paint.Style.FILL_AND_STROKE);
     }
+
 
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
-        if(visibility == 1) {
-            int paddingRight = getPaddingRight();
-            int paddingLeft=getPaddingLeft();
-            String text=getText().toString();
-            float endLineTextCenterX=0;
-            float endLineTextCenterY=0;
-            if (text!=null&&!text.isEmpty()){
-                int line=getLayout().getLineCount();
-                if (line>1){
-                    int start=getLayout().getLineStart(line-1);
-                    int end=getLayout().getLineEnd(line-1);
-                    float endLineTextWidth=getPaint().measureText(text.substring(start,end));//获取最后一行的宽度
-                    endLineTextCenterX=endLineTextWidth+paddingLeft+paddingRight;
-                    int top=getLayout().getLineTop(line-1);
-                    int bottom=getLayout().getLineBottom(line-1);
-                    endLineTextCenterY=(top+bottom)/2;
-                }else if (line==1){
-                    float endLineTextWidth=getPaint().measureText(text);
-                    endLineTextCenterX=endLineTextWidth+paddingLeft+paddingRight/2;
-                    int top=getLayout().getLineTop(0);
-                    int bottom=getLayout().getLineBottom(0);
-                    endLineTextCenterY=(top+bottom)/2;
+        if (visibility == 1) {
+//            int paddingRight = getPaddingRight();
+//            int paddingLeft = getPaddingLeft();
+            String text = getText().toString();
+            float endLineTextCenterX = 0;
+            float endLineTextCenterY = 0;
+            if (!text.isEmpty()) {
+                int line = getLayout().getLineCount();
+                if (line > 1) {
+                    int start = getLayout().getLineStart(line - 1);
+                    int end = getLayout().getLineEnd(line - 1);
+                    //float endLineTextWidth = getPaint().measureText(text.substring(start, end));//获取最后一行的宽度
+                    // endLineTextCenterX = endLineTextWidth + paddingLeft + paddingRight;
+                    endLineTextCenterX = getPaint().measureText(text.substring(start, end));
+                    int top = getLayout().getLineTop(line - 1);
+                    int bottom = getLayout().getLineBottom(line - 1);
+                    endLineTextCenterY = (top + bottom)*1.0f / 2;
+                } else if (line == 1) {
+                    //float endLineTextWidth =getPaint().measureText(text) ;
+                    //  endLineTextCenterX = endLineTextWidth + paddingLeft + paddingRight / 2;
+                    endLineTextCenterX = getPaint().measureText(text);
+                    int top = getLayout().getLineTop(0);
+                    int bottom = getLayout().getLineBottom(0);
+                    endLineTextCenterY = (top + bottom)*1.0f / 2;
                 }
-                Paint paint = new Paint();
-                paint.setColor(Color.RED);
-                paint.setAntiAlias(true);
-                paint.setDither(true);
-                paint.setStyle(Paint.Style.FILL_AND_STROKE);
-                canvas.drawCircle(endLineTextCenterX, endLineTextCenterY, pointSize /2, paint);
+
+
+                canvas.drawCircle(endLineTextCenterX, endLineTextCenterY, pointSize / 2, mPaint);
             }
 
         }
     }
 
 
-    public void setPointVisible(){
+    public void showPoint(int pointColor) {
+        this.pointColor = pointColor;
+        mPaint.setColor(pointColor);
         visibility = 1;
         invalidate();
     }
 
-    public void setPointInVisible(){
-        visibility =0;
+    public void showPoint() {
+        visibility = 1;
+        invalidate();
+    }
+
+    public void hidePoint() {
+        visibility = 0;
         invalidate();
     }
 }
