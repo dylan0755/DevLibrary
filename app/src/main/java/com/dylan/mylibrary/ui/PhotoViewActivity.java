c2b29f3a9f0ad1285a6e0d2d00f0186ab93658fb
Dylan <dylan0759@163.com>
2020-02-26 23:22:33 +0800

    BitmapHelper 新增 readBitmapDegree 读取图片角度  rotateToDegrees 旋转图片

--- app/src/main/java/com/dylan/mylibrary/ui/PhotoViewActivity.java
+++ app/src/main/java/com/dylan/mylibrary/ui/PhotoViewActivity.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 
+import com.dylan.library.widget.photoview.PhotoView;
 import com.dylan.mylibrary.R;
 
 /**
