ad1d8c9ff792b0e7f76a3ac290a7ebd8deee97ee
dylan <dylan0759@163.com>
2020-04-23 12:46:08 +0800

    缓存

--- dylanlib/src/main/java/com/dylan/library/adapter/BaseRecyclerAdapter.java
+++ dylanlib/src/main/java/com/dylan/library/adapter/BaseRecyclerAdapter.java
@@ -18,21 +18,23 @@ import java.util.ArrayList;
 import java.util.List;
 
 
-
 /**
  * Created by Dylan on 2017/1/11.
  */
 
-public abstract class BaseRecyclerAdapter<T,VH extends BaseRecyclerAdapter.ViewHolder> extends RecyclerView.Adapter {
+public abstract class BaseRecyclerAdapter<T, VH extends BaseRecyclerAdapter.ViewHolder> extends RecyclerView.Adapter {
     @NotProguard
     protected Context mContext;
     private LayoutInflater mInflater;
-   private List<T> mDataList;
+    private List<T> mDataList;
     private Constructor<VH> mSubConstrutor;
-    public BaseRecyclerAdapter(){
-        Type type=this.getClass().getGenericSuperclass();
-        if ( type instanceof ParameterizedType) {
-            ParameterizedType genericSuperclass = (ParameterizedType)type;
+    @NotProguard
+    protected OnItemClickListener mClickListener;
+
+    public BaseRecyclerAdapter() {
+        Type type = this.getClass().getGenericSuperclass();
+        if (type instanceof ParameterizedType) {
+            ParameterizedType genericSuperclass = (ParameterizedType) type;
             Type[] types = genericSuperclass.getActualTypeArguments();
             Class<VH> claszz = (Class<VH>) types[1];
             //因为是内部类，反射要传入外部类对象
@@ -48,13 +50,13 @@ public abstract class BaseRecyclerAdapter<T,VH extends BaseRecyclerAdapter.ViewH
 
     @Override
     public VH onCreateViewHolder(ViewGroup parent, int viewType) {
-        if (mContext ==null){
-            mContext =parent.getContext();
-            mInflater= LayoutInflater.from(mContext);
+        if (mContext == null) {
+            mContext = parent.getContext();
+            mInflater = LayoutInflater.from(mContext);
         }
-        View convertView=mInflater.inflate( getLayoutId(),parent,false);
+        View convertView = mInflater.inflate(getLayoutId(), parent, false);
         try {
-            VH holder= mSubConstrutor.newInstance(this,convertView);
+            VH holder = mSubConstrutor.newInstance(this, convertView);
             return holder;
         } catch (Exception e) {
             e.printStackTrace();
@@ -62,16 +64,16 @@ public abstract class BaseRecyclerAdapter<T,VH extends BaseRecyclerAdapter.ViewH
         return null;
     }
 
-    public  abstract @LayoutRes
+    public abstract @LayoutRes
     int getLayoutId();
 
-    public abstract void onBindViewHolder(VH holder,T t,int position);
+    public abstract void onBindViewHolder(VH holder, T t, int position);
 
     @Override
     public void onBindViewHolder(RecyclerView.ViewHolder holder, int position) {
-        T t=mDataList.get(position);
-        if (t!=null){
-            onBindViewHolder(((VH)holder),t,position);
+        T t = mDataList.get(position);
+        if (t != null) {
+            onBindViewHolder(((VH) holder), t, position);
         }
 
     }
@@ -86,8 +88,8 @@ public abstract class BaseRecyclerAdapter<T,VH extends BaseRecyclerAdapter.ViewH
     }
 
     public void bind(List<T> list) {
-            mDataList = list;
-            notifyDataSetChanged();
+        mDataList = list;
+        notifyDataSetChanged();
     }
 
     public void addAllAndNotifyDataChanged(List<T> list) {
@@ -100,12 +102,10 @@ public abstract class BaseRecyclerAdapter<T,VH extends BaseRecyclerAdapter.ViewH
     }
 
 
-
-
     public void notifyItemRangeChanged(List<T> list) {
-        if (list!=null){
+        if (list != null) {
             if (mDataList == null) {
-                mDataList=new ArrayList<>();
+                mDataList = new ArrayList<>();
                 mDataList.addAll(list);
                 notifyItemRangeChanged(0, list.size());
             } else {
@@ -118,46 +118,48 @@ public abstract class BaseRecyclerAdapter<T,VH extends BaseRecyclerAdapter.ViewH
     }
 
 
-
-    public void clear(){
-        if (mDataList!=null){
+    public void clear() {
+        if (mDataList != null) {
             mDataList.clear();
             notifyDataSetChanged();
         }
     }
 
-    public void loadMore(List<T> list){
-        if (list!=null&&list.size()>0){
+    public void loadMore(List<T> list) {
+        if (list != null && list.size() > 0) {
             mDataList.addAll(list);
             notifyDataSetChanged();
         }
     }
 
-    public List<T> getDataList(){
+    public List<T> getDataList() {
         return mDataList;
     }
 
 
-
-
-    public boolean isEmpty(){
-        return (mDataList==null||mDataList.size()==0)?true:false;
+    public boolean isEmpty() {
+        return (mDataList == null || mDataList.size() == 0) ? true : false;
     }
 
     @Override
     public int getItemCount() {
-        return (mSubConstrutor==null||mDataList==null)?0:(mDataList.size());
+        return (mSubConstrutor == null || mDataList == null) ? 0 : (mDataList.size());
     }
 
 
-
-    public static class ViewHolder extends RecyclerView.ViewHolder{
+    public static class ViewHolder extends RecyclerView.ViewHolder {
         public ViewHolder(View itemView) {
             super(itemView);
 
         }
 
+    }
 
+    public interface OnItemClickListener<T> {
+        void onClick(T t,int position);
+    }
 
+    public void setOnItemClickListener(OnItemClickListener<T> listener){
+         mClickListener=listener;
     }
 }
