ded70e669490763e5b370370a1fa78f0a5919f5e
dylan <dylan0759@163.com>
2020-04-28 09:33:45 +0800

    SoftKeyboardUtils 新增  AdjustPan ，调整页面输入框被软键盘遮挡问题

--- dylanlib/src/main/java/com/dylan/library/utils/SoftKeyboardUtils.java
+++ dylanlib/src/main/java/com/dylan/library/utils/SoftKeyboardUtils.java
@@ -11,6 +11,7 @@ import android.view.ViewGroup;
 import android.view.ViewTreeObserver;
 import android.view.inputmethod.InputMethodManager;
 import android.widget.EditText;
+import android.widget.FrameLayout;
 import android.widget.LinearLayout;
 import android.widget.ScrollView;
 
@@ -20,6 +21,14 @@ import com.dylan.library.screen.ScreenUtils;
  * Created by Dylan on 2016/10/9.
  */
 public class SoftKeyboardUtils {
+
+
+    public static void assistActivity(Activity activity) {
+        new AdjustPan(activity);
+    }
+
+
+
     public static void observeSoftKeyboard(Activity activity, final OnSoftKeyboardChangeListener listener) {
         final View decorView = activity.getWindow().getDecorView();
         ViewTreeObserver.OnGlobalLayoutListener onGlobalLayoutListener=  new ViewTreeObserver.OnGlobalLayoutListener() {
@@ -125,5 +134,80 @@ public class SoftKeyboardUtils {
 
             }
         });
+    }
+
+
+
+    public static class AdjustPan{
+
+        private View mChildOfContent;
+        private int usableHeightPrevious;
+        private FrameLayout.LayoutParams frameLayoutParams;
+        private int contentHeight;
+        private boolean isfirst = true;
+        private Activity activity;
+        private int statusBarHeight;
+
+
+        private AdjustPan(Activity activity) {
+            //获取状态栏的高度
+            int resourceId = activity.getResources().getIdentifier("status_bar_height", "dimen", "android");
+            statusBarHeight = activity.getResources().getDimensionPixelSize(resourceId);
+            this.activity = activity;
+            FrameLayout content = (FrameLayout) activity.findViewById(android.R.id.content);
+            mChildOfContent = content.getChildAt(0);
+
+            //界面出现变动都会调用这个监听事件
+            mChildOfContent.getViewTreeObserver().addOnGlobalLayoutListener(new ViewTreeObserver.OnGlobalLayoutListener() {
+                public void onGlobalLayout() {
+                    if (isfirst) {
+                        contentHeight = mChildOfContent.getHeight();//兼容华为等机型
+                        isfirst = false;
+                    }
+                    possiblyResizeChildOfContent();
+                }
+            });
+
+            frameLayoutParams = (FrameLayout.LayoutParams)
+                    mChildOfContent.getLayoutParams();
+        }
+
+        //重新调整跟布局的高度
+        private void possiblyResizeChildOfContent() {
+
+            int usableHeightNow = computeUsableHeight();
+
+            //当前可见高度和上一次可见高度不一致 布局变动
+            if (usableHeightNow != usableHeightPrevious) {
+                //int usableHeightSansKeyboard2 = mChildOfContent.getHeight();//兼容华为等机型
+                int usableHeightSansKeyboard = mChildOfContent.getRootView().getHeight();
+                int heightDifference = usableHeightSansKeyboard - usableHeightNow;
+                if (heightDifference > (usableHeightSansKeyboard / 4)) {
+                    // keyboard probably just became visible
+                    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+                        //frameLayoutParams.height = usableHeightSansKeyboard - heightDifference;
+                        frameLayoutParams.height = usableHeightSansKeyboard - heightDifference + statusBarHeight;
+                    } else {
+                        frameLayoutParams.height = usableHeightSansKeyboard - heightDifference;
+                    }
+                } else {
+                    frameLayoutParams.height = contentHeight;
+                }
+
+                mChildOfContent.requestLayout();
+                usableHeightPrevious = usableHeightNow;
+            }
+        }
+
+        /**
+         * 计算mChildOfContent可见高度     ** @return
+         */
+        private int computeUsableHeight() {
+            Rect r = new Rect();
+            mChildOfContent.getWindowVisibleDisplayFrame(r);
+            return (r.bottom - r.top);
+        }
+
+
     }
 }
