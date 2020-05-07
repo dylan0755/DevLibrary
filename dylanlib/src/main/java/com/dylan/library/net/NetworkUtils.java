7640b70b3c0132c830b0fad9f8eb43e213fb21fc
dylan <dylan0759@163.com>
2020-05-07 16:45:56 +0800

    更新NetworkUtils 使用兼容方式获取网络状态

---
+++ dylanlib/src/main/java/com/dylan/library/net/NetworkUtils.java
@@ -0,0 +1,84 @@
+package com.dylan.library.net;
+
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkCapabilities;
+import android.net.NetworkInfo;
+import android.os.Build;
+import android.support.annotation.NonNull;
+
+import static android.Manifest.permission.ACCESS_NETWORK_STATE;
+
+
+/**
+ * 添加 ACCESS_NETWORK_STATE ACCESS_WIFI_STATE 两个权限
+ */
+public class NetworkUtils {
+
+  /**
+   * 网络是否已连接
+   *
+   * @return true:已连接 false:未连接
+   */
+  @SuppressWarnings("deprecation")
+  public static boolean iConnected(@NonNull Context context) {
+    ConnectivityManager manager = (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
+    if (manager != null) {
+      if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+        NetworkCapabilities networkCapabilities = manager.getNetworkCapabilities(manager.getActiveNetwork());
+        if (networkCapabilities != null) {
+          return networkCapabilities.hasTransport(NetworkCapabilities.TRANSPORT_WIFI)
+                  || networkCapabilities.hasTransport(NetworkCapabilities.TRANSPORT_CELLULAR)
+                  || networkCapabilities.hasTransport(NetworkCapabilities.TRANSPORT_ETHERNET);
+        }
+      } else {
+        NetworkInfo networkInfo = manager.getActiveNetworkInfo();
+        return networkInfo != null && networkInfo.isConnected();
+      }
+    }
+    return false;
+  }
+
+  /**
+   * Wifi是否已连接
+   *
+   * @return true:已连接 false:未连接
+   */
+  @SuppressWarnings("deprecation")
+  public static boolean isWifiConnected(@NonNull Context context) {
+    ConnectivityManager manager = (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
+    if (manager != null) {
+      if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+        NetworkCapabilities networkCapabilities = manager.getNetworkCapabilities(manager.getActiveNetwork());
+        if (networkCapabilities != null) {
+          return networkCapabilities.hasTransport(NetworkCapabilities.TRANSPORT_WIFI);
+        }
+      } else {
+        NetworkInfo networkInfo = manager.getActiveNetworkInfo();
+        return networkInfo != null && networkInfo.isConnected() && networkInfo.getType() == ConnectivityManager.TYPE_WIFI;
+      }
+    }
+    return false;
+  }
+
+  /**
+   * 是否为流量
+   */
+  @SuppressWarnings("deprecation")
+  public static boolean isMobileData(@NonNull Context context) {
+    ConnectivityManager manager = (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
+    if (manager != null) {
+      if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+        NetworkCapabilities networkCapabilities = manager.getNetworkCapabilities(manager.getActiveNetwork());
+        if (networkCapabilities != null) {
+          return networkCapabilities.hasTransport(NetworkCapabilities.TRANSPORT_CELLULAR);
+        }
+      } else {
+        NetworkInfo networkInfo = manager.getActiveNetworkInfo();
+        return networkInfo != null && networkInfo.isConnected() && networkInfo.getType() == ConnectivityManager.TYPE_MOBILE;
+      }
+    }
+    return false;
+  }
+
+}
