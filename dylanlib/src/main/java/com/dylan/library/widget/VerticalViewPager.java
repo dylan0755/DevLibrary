40965f2260d316c99c04f6b2dff4c9e4b6d8cbef
Dylan <dylan0759@163.com>
2020-07-19 13:31:02 +0800

    新增 VerticalViewPager

---
+++ dylanlib/src/main/java/com/dylan/library/widget/VerticalViewPager.java
@@ -0,0 +1,78 @@
+package com.dylan.library.widget;
+
+import android.content.Context;
+import android.support.v4.view.ViewPager;
+import android.util.AttributeSet;
+import android.view.MotionEvent;
+import android.view.View;
+
+/**
+ * Author: Administrator
+ * Date: 2020/7/13
+ * Desc:
+ */
+public class VerticalViewPager extends ViewPager {
+
+    public VerticalViewPager(Context context) {
+        this(context, null);
+    }
+
+    public VerticalViewPager(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        //设置viewpage的切换动画,这里设置才能真正实现垂直滑动的viewpager
+        setPageTransformer(true, new DefaultTransformer());
+    }
+
+    /**
+     * 拦截touch事件
+     *
+     */
+    @Override
+    public boolean onInterceptTouchEvent(MotionEvent ev) {
+        boolean intercept = super.onInterceptTouchEvent(swapEvent(ev));
+        swapEvent(ev);
+        return intercept;
+    }
+
+
+    @Override
+    public boolean onTouchEvent(MotionEvent ev) {
+        return super.onTouchEvent(swapEvent(ev));
+    }
+
+    private MotionEvent swapEvent(MotionEvent event) {
+        //获取宽高
+        float width = getWidth();
+        float height = getHeight();
+        //将Y轴的移动距离转变成X轴的移动距离
+        float swappedX = (event.getY() / height) * width;
+        //将X轴的移动距离转变成Y轴的移动距离
+        float swappedY = (event.getX() / width) * height;
+        //重设event的位置
+        event.setLocation(swappedX, swappedY);
+        return event;
+    }
+
+
+
+
+    public class DefaultTransformer implements ViewPager.PageTransformer {
+
+
+        @Override
+        public void transformPage(View view, float position) {
+
+            float alpha = 0;
+            if (0 <= position && position <= 1) {
+                alpha = 1 - position;
+            } else if (-1 < position && position < 0) {
+                alpha = position + 1;
+            }
+            view.setAlpha(alpha);
+            float transX = view.getWidth() * -position;
+            view.setTranslationX(transX);
+            float transY = position * view.getHeight();
+            view.setTranslationY(transY);
+        }
+    }
+}
