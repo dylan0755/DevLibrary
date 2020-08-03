1681a72f92274417fadc87fd24376f65f66efbf0
dylan <dylan0759@163.com>
2020-08-03 11:20:53 +0800

    hideNavBarFromWindowAndDecorView 键盘缩下去的时候导航栏自动收缩

--- dylanlib/src/main/java/com/dylan/library/screen/NavBarUtils.java
+++ dylanlib/src/main/java/com/dylan/library/screen/NavBarUtils.java
@@ -18,53 +18,31 @@ import java.lang.reflect.Method;
 
 public class NavBarUtils {
 
-    /**
-     * 检查是否存在虚拟按键栏
-     * @param context
-     * @return
-     */
-    public static boolean hasNavBar(Context context) {
-        try{
-            Resources res = context.getResources();
-            int resourceId = res.getIdentifier("config_showNavigationBar", "bool", "android");
-            if (resourceId != 0) {
-                boolean hasNav = res.getBoolean(resourceId);
-                // check override flag
-                String sNavBarOverride = getNavBarOverride();
-                if ("1".equals(sNavBarOverride)) {
-                    hasNav = false;
-                } else if ("0".equals(sNavBarOverride)) {
-                    hasNav = true;
-                }
-                return hasNav;
-            } else { // fallback
-                return !ViewConfiguration.get(context).hasPermanentMenuKey();
-            }
-        }catch (Exception e){
 
-        }
-
-        return false;
-
+    public static void hideNavBarFromWindowAndDecorView(Window window){
+        hideNavBarFromWindow(window);
+        hideNavBarFromDecorView(window);
     }
 
-    /**
-     * 判断虚拟按键栏是否重写
-     * @return
-     */
-    private static String getNavBarOverride() {
-        String sNavBarOverride = null;
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
-            try {
-                Class c = Class.forName("android.os.SystemProperties");
-                Method m = c.getDeclaredMethod("get", String.class);
-                m.setAccessible(true);
-                sNavBarOverride = (String) m.invoke(null, "qemu.hw.mainkeys");
-            } catch (Throwable e) {
-            }
-        }
-        return sNavBarOverride;
+
+    //从Window层隐藏，键盘缩下去的时候导航栏自动收缩
+    public static void hideNavBarFromWindow(Window window){
+        WindowManager.LayoutParams params = window.getAttributes();
+        params.systemUiVisibility = View.SYSTEM_UI_FLAG_HIDE_NAVIGATION;
+        window.setAttributes(params);
     }
+
+    //从DecorView 层隐藏，键盘上的收缩角标按下时，键盘收缩，但监听不到，所以要和hideNavBarFromWindow 搭配使用
+    public static void hideNavBarFromDecorView(Window window){
+        hideNavBar(window);
+    }
+
+
+
+
+
+
+
     /**
      * 隐藏虚拟按键，并且全屏
      */
@@ -128,6 +106,8 @@ public class NavBarUtils {
     }
 
 
+
+
     public static int getNavBarHeight(Context context) {
         boolean mInPortrait = context.getResources().getConfiguration().orientation == Configuration.ORIENTATION_PORTRAIT;
         int result = 0;
@@ -159,5 +139,57 @@ public class NavBarUtils {
             return 0;
         }
         return result;
+    }
+
+
+
+
+
+    /**
+     * 检查是否存在虚拟按键栏
+     * @param context
+     * @return
+     */
+    public static boolean hasNavBar(Context context) {
+        try{
+            Resources res = context.getResources();
+            int resourceId = res.getIdentifier("config_showNavigationBar", "bool", "android");
+            if (resourceId != 0) {
+                boolean hasNav = res.getBoolean(resourceId);
+                // check override flag
+                String sNavBarOverride = getNavBarOverride();
+                if ("1".equals(sNavBarOverride)) {
+                    hasNav = false;
+                } else if ("0".equals(sNavBarOverride)) {
+                    hasNav = true;
+                }
+                return hasNav;
+            } else { // fallback
+                return !ViewConfiguration.get(context).hasPermanentMenuKey();
+            }
+        }catch (Exception e){
+
+        }
+
+        return false;
+
+    }
+
+    /**
+     * 判断虚拟按键栏是否重写
+     * @return
+     */
+    private static String getNavBarOverride() {
+        String sNavBarOverride = null;
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+            try {
+                Class c = Class.forName("android.os.SystemProperties");
+                Method m = c.getDeclaredMethod("get", String.class);
+                m.setAccessible(true);
+                sNavBarOverride = (String) m.invoke(null, "qemu.hw.mainkeys");
+            } catch (Throwable e) {
+            }
+        }
+        return sNavBarOverride;
     }
 }
