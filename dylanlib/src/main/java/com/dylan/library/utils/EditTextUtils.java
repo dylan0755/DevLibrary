a22bf976bd580fa74414c2da4551c954dcf3e63e
dylan <dylan0759@163.com>
2020-08-12 16:30:02 +0800

    EditTextUtils 新增 parseDoubleValue 和 parseIntValue

--- dylanlib/src/main/java/com/dylan/library/utils/EditTextUtils.java
+++ dylanlib/src/main/java/com/dylan/library/utils/EditTextUtils.java
@@ -20,8 +20,31 @@ import android.widget.TextView;
 
 public class EditTextUtils {
 
+
+    public static double parseDoubleValue(TextView textView) {
+        double value = 0;
+        try {
+            value = Double.parseDouble(textView.getText().toString());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return value;
+    }
+
+
+    public static int parseIntValue(TextView textView) {
+        int value = 0;
+        try {
+            value = Integer.parseInt(textView.getText().toString());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return value;
+    }
+
+
     //保留小数点
-    public static void keepDecimal(final EditText editText,final  int decimalCount){
+    public static void keepDecimal(final EditText editText, final int decimalCount) {
 
         editText.addTextChangedListener(new TextWatcher() {
             @Override
@@ -33,7 +56,7 @@ public class EditTextUtils {
             public void onTextChanged(CharSequence s, int start, int before, int count) {
                 if (s.toString().contains(".")) {
                     if (s.length() - 1 - s.toString().indexOf(".") > decimalCount) {
-                        s = s.toString().subSequence(0, s.toString().indexOf(".") +decimalCount+1);
+                        s = s.toString().subSequence(0, s.toString().indexOf(".") + decimalCount + 1);
                         editText.setText(s);
                         editText.setSelection(s.length());
                         return;
@@ -43,7 +66,7 @@ public class EditTextUtils {
 
             @Override
             public void afterTextChanged(Editable s) {
-                if (null == s)  return;
+                if (null == s) return;
 
                 if (".".equals(s.toString())) {
                     editText.setText("0.");
@@ -59,7 +82,6 @@ public class EditTextUtils {
             }
         });
     }
-
 
 
     public static void setGravityCenter(EditText editText) {
@@ -91,11 +113,11 @@ public class EditTextUtils {
 
 
     public static void hideCursor(EditText editText) {
-        if (editText!=null)editText.setCursorVisible(false);
+        if (editText != null) editText.setCursorVisible(false);
     }
 
-    public static void showCursor(EditText editText){
-        if (editText!=null)editText.setCursorVisible(true);
+    public static void showCursor(EditText editText) {
+        if (editText != null) editText.setCursorVisible(true);
     }
 
 
@@ -116,29 +138,28 @@ public class EditTextUtils {
     }
 
 
-
-    public static void addDoneAction(EditText editText, final OnDoneActionCallBack callBack){
+    public static void addDoneAction(EditText editText, final OnDoneActionCallBack callBack) {
         editText.setImeActionLabel("完成", EditorInfo.IME_ACTION_DONE);
-        editText.setImeOptions(EditorInfo.IME_ACTION_DONE|EditorInfo.IME_FLAG_NO_EXTRACT_UI);//优化,横屏全屏输入法不占满屏
-        addOnEditorActionListener(editText,callBack);
+        editText.setImeOptions(EditorInfo.IME_ACTION_DONE | EditorInfo.IME_FLAG_NO_EXTRACT_UI);//优化,横屏全屏输入法不占满屏
+        addOnEditorActionListener(editText, callBack);
     }
 
 
-    public static void addSearchAction(EditText editText, final OnDoneActionCallBack callBack){
-        editText.setImeActionLabel("搜索",EditorInfo.IME_ACTION_SEARCH);
-        editText.setImeOptions(EditorInfo.IME_ACTION_SEARCH|EditorInfo.IME_FLAG_NO_EXTRACT_UI);//优化,横屏全屏输入法不占满屏
-        addOnEditorActionListener(editText,callBack);
+    public static void addSearchAction(EditText editText, final OnDoneActionCallBack callBack) {
+        editText.setImeActionLabel("搜索", EditorInfo.IME_ACTION_SEARCH);
+        editText.setImeOptions(EditorInfo.IME_ACTION_SEARCH | EditorInfo.IME_FLAG_NO_EXTRACT_UI);//优化,横屏全屏输入法不占满屏
+        addOnEditorActionListener(editText, callBack);
     }
 
 
-    public static void addSendAction(EditText editText, final OnDoneActionCallBack callBack){
-        editText.setImeActionLabel("发送",EditorInfo.IME_ACTION_SEND);
-        editText.setImeOptions(EditorInfo.IME_ACTION_SEND|EditorInfo.IME_FLAG_NO_EXTRACT_UI);//优化,横屏全屏输入法不占满屏
-        addOnEditorActionListener(editText,callBack);
+    public static void addSendAction(EditText editText, final OnDoneActionCallBack callBack) {
+        editText.setImeActionLabel("发送", EditorInfo.IME_ACTION_SEND);
+        editText.setImeOptions(EditorInfo.IME_ACTION_SEND | EditorInfo.IME_FLAG_NO_EXTRACT_UI);//优化,横屏全屏输入法不占满屏
+        addOnEditorActionListener(editText, callBack);
     }
 
 
-    private static void addOnEditorActionListener(EditText editText, final OnDoneActionCallBack callBack){
+    private static void addOnEditorActionListener(EditText editText, final OnDoneActionCallBack callBack) {
         editText.setOnEditorActionListener(new TextView.OnEditorActionListener() {
 
             @Override
@@ -148,10 +169,10 @@ public class EditTextUtils {
                 //注意，这是一定要判断event != null。因为在某些输入法上会返回null。
                 if (actionId == EditorInfo.IME_ACTION_SEARCH
                         || actionId == EditorInfo.IME_ACTION_DONE
-                        ||actionId==EditorInfo.IME_ACTION_SEND
+                        || actionId == EditorInfo.IME_ACTION_SEND
                         || (event != null && KeyEvent.KEYCODE_ENTER == event.getKeyCode() && KeyEvent.ACTION_DOWN == event.getAction())) {
                     SoftKeyboardUtils.hideSoftInput(v);
-                    if (callBack!=null)callBack.onKeyEvent(v,actionId,event);
+                    if (callBack != null) callBack.onKeyEvent(v, actionId, event);
                 }
                 return true;
             }
@@ -162,7 +183,6 @@ public class EditTextUtils {
     public interface OnDoneActionCallBack {
         void onKeyEvent(TextView v, int actionId, KeyEvent event);
     }
-
 
 
 }
