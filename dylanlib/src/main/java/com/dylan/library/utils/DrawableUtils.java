9d9da672c4ef250158154add9c07d435dbb8883c
dylan <dylan0759@163.com>
2020-08-22 10:57:47 +0800

    DrawableUtils  setBackgroundMutateAlpha 避免更改一处布局透明度引起其它地方改变

--- dylanlib/src/main/java/com/dylan/library/utils/DrawableUtils.java
+++ dylanlib/src/main/java/com/dylan/library/utils/DrawableUtils.java
@@ -1,5 +1,6 @@
 package com.dylan.library.utils;
 
+import android.graphics.drawable.Drawable;
 import android.graphics.drawable.GradientDrawable;
 import android.view.View;
 
@@ -14,9 +15,17 @@ public class DrawableUtils {
     public static void resetShapeColor(View view, int backgroundColor) {
         if (view == null) return;
         GradientDrawable gradientDrawable = (GradientDrawable) view.getBackground();
-        if (gradientDrawable != null){
+        if (gradientDrawable != null) {
             gradientDrawable.mutate();
             gradientDrawable.setColor(backgroundColor);
         }
+    }
+
+
+    public static void setBackgroundMutateAlpha(View view, int alpha) {
+        if (view==null||view.getBackground()==null)return;
+        Drawable drawable = view.getBackground();
+        drawable.mutate();
+        drawable.setAlpha(alpha);
     }
 }
