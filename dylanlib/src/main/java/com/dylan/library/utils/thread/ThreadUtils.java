fc23561e451034d382b76fa5064a0a9fb3bdbee0
Dylan <dylan0759@163.com>
2021-01-28 18:30:25 +0800

    save

--- dylanlib/src/main/java/com/dylan/library/utils/thread/ThreadUtils.java
+++ dylanlib/src/main/java/com/dylan/library/utils/thread/ThreadUtils.java
@@ -26,6 +26,19 @@ public class ThreadUtils {
 		}
 	}
 
+	public static  void runOnMainThreadDelay(Runnable action,long delayMillis){
+		try {
+			Class<?> clazz = Class.forName("android.app.ActivityThread");
+			// 其实这货就是ActivityThread.mH这个贼重要的Handler(用于处理各种Client端的组件与系统服务端进行消息交互等等)
+			Field field = clazz.getDeclaredField("sMainThreadHandler");
+			field.setAccessible(true);
+			Handler sMainThreadHandler = (Handler) field.get(null);
+			sMainThreadHandler.postDelayed(action,delayMillis);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
 
 
 }
