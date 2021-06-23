2fab3b0b84c3ee3914cc5352a8ba4d31b47a43ed
Dylan <dylan0759@163.com>
2021-06-23 18:25:45 +0800

    save

--- dylanlib/src/main/java/com/dylan/library/utils/StringUtils.java
+++ dylanlib/src/main/java/com/dylan/library/utils/StringUtils.java
@@ -186,6 +186,31 @@ public class StringUtils {
         return false;
     }
 
+    public static boolean isMD5(String msg) {
+        int cnt = 0;
+        for (int i=0; i<msg.length(); ++i) {
+            switch (msg.charAt(i)) {
+                case '0': case '1': case '2': case '3': case '4':
+                case '5': case '6': case '7': case '8': case '9':
+                case 'a': case 'b': case 'c': case 'd': case 'e': case 'f':
+                case 'A': case 'B': case 'C': case 'D': case 'E': case 'F':
+                    ++ cnt;
+                    if (32 <= cnt) return true;
+                    break;
+                case '/':
+                    if ((i + 10) < msg.length()) {// "/storage/"
+                        char ch1 = msg.charAt(i+1);
+                        char ch2 = msg.charAt(i+8);
+                        if ('/' == ch2 && ('s' == ch1 || 'S' == ch1)) return true;
+                    }
+                default:
+                    cnt = 0;
+                    break;
+            }
+        }
+        return false;
+    }
+
 
     /**
      * 判断是否包含中文，支持 。 ，中文字符
