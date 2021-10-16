ec84417ae72dade8a2c0246aadfdaa06a8cc3e59
dylan0755 <dylan0759@163.com>
2021-10-16 16:00:25 +0800

    save

--- dylanlib/src/main/java/com/dylan/library/widget/RoundCropTopImageView.java
+++ dylanlib/src/main/java/com/dylan/library/widget/RoundCropTopImageView.java
@@ -24,26 +24,40 @@ public class RoundCropTopImageView extends RoundImageView {
 
     public RoundCropTopImageView(Context context, AttributeSet attrs, int defStyle) {
         super(context, attrs, defStyle);
+        setScaleType(ScaleType.CENTER_CROP);
         setScaleType(ScaleType.MATRIX);
     }
 
     @Override
-    protected boolean setFrame(int l, int t, int r, int b)
+    protected boolean setFrame(int frameLeft, int frameTop, int frameRight, int frameBottom)
     {
         if (getDrawable() == null) {
-            return super.setFrame(l, t, r, b);
+            return super.setFrame(frameLeft, frameTop, frameRight, frameBottom);
         }
+        float frameWidth = frameRight - frameLeft;
+        float frameHeight = frameBottom - frameTop;
+
+        float originalImageWidth = (float) getDrawable().getIntrinsicWidth();
+        float originalImageHeight = (float) getDrawable().getIntrinsicHeight();
+
+        float usedScaleFactor = 1;
+
+        if ((frameWidth > originalImageWidth) || (frameHeight > originalImageHeight)) {
+            float fitHorizontallyScaleFactor = frameWidth / originalImageWidth;
+            float fitVerticallyScaleFactor = frameHeight / originalImageHeight;
+
+            usedScaleFactor = Math.max(fitHorizontallyScaleFactor, fitVerticallyScaleFactor);
+        }
+
+        float newImageWidth = originalImageWidth * usedScaleFactor;
+        float newImageHeight = originalImageHeight * usedScaleFactor;
+
         Matrix matrix = getImageMatrix();
-        float scaleWidth = getWidth()/(float)getDrawable().getIntrinsicWidth();
-        float scaleHeight = getHeight()/(float)getDrawable().getIntrinsicHeight();
-        float scaleFactor = (scaleWidth > scaleHeight) ? scaleWidth : scaleHeight;
-        matrix.setScale(scaleFactor, scaleFactor, 0, 0);
-        if (scaleFactor == scaleHeight) {
-            float tanslateX = ((getDrawable().getIntrinsicWidth() * scaleFactor) - getWidth()) / 2;
-            matrix.postTranslate(-tanslateX, 0);
-        }
+        matrix.setScale(usedScaleFactor, usedScaleFactor, 0, 0); // Replaces the old matrix completly
+        // matrix.postTranslate((frameWidth - newImageWidth) / 2, frameHeight - newImageHeight);//BottomCrop
+        matrix.postTranslate((frameWidth - newImageWidth) / 2, 0);//Top Crop
         setImageMatrix(matrix);
-        return super.setFrame(l, t, r, b);
+        return super.setFrame(frameLeft, frameTop, frameRight, frameBottom);
     }
 
 
