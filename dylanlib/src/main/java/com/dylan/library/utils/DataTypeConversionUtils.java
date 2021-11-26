ad1af3545677cf95be95aa1e2ca8a123d2d29f30
dylan0755 <dylan0759@163.com>
2021-11-26 20:52:35 +0800

    新增DataTypeConversionUtils 数据类型转换工具

---
+++ dylanlib/src/main/java/com/dylan/library/utils/DataTypeConversionUtils.java
@@ -0,0 +1,56 @@
+package com.dylan.library.utils;
+
+import java.nio.ByteOrder;
+
+/**
+ * Author: Dylan
+ * Date: 2021/11/26
+ * Desc:
+ */
+public class DataTypeConversionUtils {
+
+    public short[] byteArrayToShortArray(byte[] buf) {
+        byte bLength = 2;
+        short[] s = new short[buf.length / bLength];
+        for (int iLoop = 0; iLoop < s.length; iLoop++) {
+            byte[] temp = new byte[bLength];
+            for (int jLoop = 0; jLoop < bLength; jLoop++) {
+                temp[jLoop] = buf[iLoop * bLength + jLoop];
+            }
+            s[iLoop] = getShort(temp,testCPU());
+        }
+        return s;
+    }
+    public boolean testCPU() {
+        if (ByteOrder.nativeOrder() == ByteOrder.BIG_ENDIAN) {
+            // System.out.println("is big ending");
+            return true;
+        } else {
+            // System.out.println("is little ending");
+            return false;
+        }
+    }
+
+    public short getShort(byte[] buf, boolean bBigEnding) {
+        if (buf == null) {
+            throw new IllegalArgumentException("byte array is null!");
+        }
+        if (buf.length > 2) {
+            throw new IllegalArgumentException("byte array size > 2 !");
+        }
+        short r = 0;
+        if (bBigEnding) {
+            for (int i = 0; i < buf.length; i++) {
+                r <<= 8;
+                r |= (buf[i] & 0x00ff);
+            }
+        } else {
+            for (int i = buf.length - 1; i >= 0; i--) {
+                r <<= 8;
+                r |= (buf[i] & 0x00ff);
+            }
+        }
+
+        return r;
+    }
+}
