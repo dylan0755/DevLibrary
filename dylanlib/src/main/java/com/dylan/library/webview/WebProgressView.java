bb0ea703622d8f70381ed5b1d8e3c2d20ffeac9e
Dylan <dylan0759@163.com>
2022-03-27 14:24:35 +0800

    save

--- dylanlib/src/main/java/com/dylan/library/webview/WebProgressView.java
+++ dylanlib/src/main/java/com/dylan/library/webview/WebProgressView.java
@@ -7,8 +7,6 @@ import android.graphics.Paint;
 import android.util.AttributeSet;
 import android.view.View;
 
-import com.dylan.library.screen.ScaleUtils;
-
 /**
  * Created by admin on 2017/6/17
  */
@@ -16,8 +14,7 @@ public class WebProgressView extends View {
 
     private Paint paint;//进度条的画笔
     private int progress = 1;//进度默认为1
-    private  static int HEIGHT ;//进度条高度为5
-
+    private int height;
     public WebProgressView(Context context) {
         this(context, null);
     }
@@ -31,11 +28,16 @@ public class WebProgressView extends View {
         initPaint(context);
     }
 
+    @Override
+    protected void onLayout(boolean changed, int left, int top, int right, int bottom) {
+        super.onLayout(changed, left, top, right, bottom);
+        height=getMeasuredHeight();
+        paint.setStrokeWidth(height);
+    }
+
     private void initPaint(Context context) {
-        HEIGHT=new ScaleUtils(context).toScaleSize(3);
         paint = new Paint(Paint.DITHER_FLAG);
         paint.setStyle(Paint.Style.STROKE);//填充方式为描边
-        paint.setStrokeWidth(HEIGHT);//设置画笔的宽度
         paint.setAntiAlias(true);//抗锯齿
         paint.setDither(true);//使用抖动效果
         paint.setColor(Color.parseColor("#f15701"));//画笔设置颜色
@@ -51,14 +53,10 @@ public class WebProgressView extends View {
         paint.setColor(colorValue);
     }
 
-    public void setHeight(int px){
-        HEIGHT=new ScaleUtils(getContext()).toScaleSize(px);
-        paint.setStrokeWidth(HEIGHT);//设置画笔的宽度
-    }
 
     @Override
     protected void onDraw(Canvas canvas) {
-        canvas.drawRect(0, 0, getWidth() * progress / 100, HEIGHT, paint);//画矩形从（0.0）开始到(progress,height)的区域
+        canvas.drawRect(0, 0, getWidth() * progress / 100, height, paint);//画矩形从（0.0）开始到(progress,height)的区域
     }
 
 }
