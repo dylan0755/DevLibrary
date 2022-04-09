70ad8d352b651e9b6f4f4b2ff06f2199726522dc
dylan0755 <dylan0759@163.com>
2022-04-09 16:38:59 +0800

    新增 MediumEditText

---
+++ dylanlib/src/main/java/com/dylan/library/widget/MediumEditText.java
@@ -0,0 +1,51 @@
+package com.dylan.library.widget;
+
+import android.content.Context;
+import android.content.res.TypedArray;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.support.v7.widget.AppCompatEditText;
+import android.support.v7.widget.AppCompatTextView;
+import android.text.TextPaint;
+import android.util.AttributeSet;
+
+import com.dylan.library.R;
+
+/**
+ * Author: Dylan
+ * Date: 2021/1/30
+ * Desc:
+ */
+
+public class MediumEditText extends AppCompatEditText {
+    private float mStrokeWidth=0.5f;//0.0f表示常规画笔的宽度，相当于默认情况。
+
+    public MediumEditText(Context context) {
+        this(context, null);
+    }
+
+    public MediumEditText(Context context, AttributeSet attrs) {
+        this(context, attrs, 0);
+    }
+
+    public MediumEditText(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        TypedArray array = context.obtainStyledAttributes(attrs, R.styleable.MediumTextView, defStyleAttr, 0);
+        mStrokeWidth = array.getFloat(R.styleable.MediumTextView_textBold, mStrokeWidth);
+        array.recycle();
+    }
+
+
+    public void setBold(float fraction){
+        mStrokeWidth=fraction;
+        invalidate();
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        TextPaint textPaint=getPaint();
+        textPaint.setStrokeWidth(mStrokeWidth);
+        textPaint.setStyle(Paint.Style.FILL_AND_STROKE);
+        super.onDraw(canvas);
+    }
+}
