c95146829ac7dd800984cf5539e024130a6448f7
Dylan <dylan0759@163.com>
2022-04-13 23:57:12 +0800

    save

--- app/src/main/java/com/dylan/mylibrary/ui/ShadowLayoutActivity.java
+++ app/src/main/java/com/dylan/mylibrary/ui/ShadowLayoutActivity.java
@@ -1,9 +1,13 @@
 package com.dylan.mylibrary.ui;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.LinearLayout;
 
+import com.dylan.library.utils.DensityUtils;
+import com.dylan.library.widget.shape.ShadowDrawable;
 import com.dylan.mylibrary.R;
 
 /**
@@ -16,5 +20,11 @@ public class ShadowLayoutActivity extends AppCompatActivity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_shadowlayout);
+        LinearLayout layout3=findViewById(R.id.layout3);
+
+
+        // 实例：设置背景为颜色为#3D5AFE，圆角为8dp, 阴影颜色为#66000000，宽度为10dp的背景
+        ShadowDrawable.setShadowDrawable(layout3, Color.WHITE, DensityUtils.dp2px(this,8),
+                Color.parseColor("#66000000"), DensityUtils.dp2px(this,10), 0, 0);
     }
 }
