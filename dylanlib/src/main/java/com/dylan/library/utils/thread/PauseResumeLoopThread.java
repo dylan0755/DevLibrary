3e798f7995d8d7090b53028c886684dc4684471d
Dylan <dylan0759@163.com>
2022-04-23 17:19:55 +0800

    新增 PauseResumeLoopThread，控制线程的暂停和恢复

---
+++ dylanlib/src/main/java/com/dylan/library/utils/thread/PauseResumeLoopThread.java
@@ -0,0 +1,61 @@
+package com.dylan.library.utils.thread;
+
+
+/**
+ * Author: Dylan
+ * Date: 2022/4/23
+ * Desc: 控制线程的暂停和恢复运行
+ */
+
+public abstract class PauseResumeLoopThread extends Thread{
+    private boolean mRunning=true;
+    private final Object lock = new Object();
+    private boolean pause = false;
+
+    public void resumeThread(){
+        //线程恢复
+        synchronized (lock) {
+            pause = false;
+            lock.notifyAll();
+        }
+    }
+
+    public boolean isPause() {
+        return pause;
+    }
+
+    public void pauseThread(){
+        pause=true;
+    }
+
+
+
+    public void exitThread(){
+        mRunning=false;
+    }
+
+
+    //这个方法只能在子线程中的run方法重调用，不然会阻塞主线程
+    private void pause(){
+        synchronized (lock) {
+            try {
+                lock.wait();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    @Override
+    public void run() {
+        while(mRunning){
+            if (pause){
+                pause();
+            }
+            doRun();
+        }
+    }
+
+
+    public abstract void doRun();
+}
