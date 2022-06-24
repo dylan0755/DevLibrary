0dbaeb8783eece923e2d57d30d140f3c141ee656
dylan0755 <dylan0759@163.com>
2022-06-24 11:39:45 +0800

    新增 ScrollHorizontalRecyclerView，解决ScrollView 中嵌套RecyclerView 横向滑动事件冲突问题

---
+++ dylanlib/src/main/java/com/dylan/library/widget/irecycler/ScrollHorizontalRecyclerView.java
@@ -0,0 +1,62 @@
+package com.dylan.library.widget.irecycler;
+
+import android.content.Context;
+import android.support.annotation.Nullable;
+import android.support.v7.widget.RecyclerView;
+import android.util.AttributeSet;
+import android.view.MotionEvent;
+
+/**
+ * Author: Dylan
+ * Date: 2022/06/24
+ * Desc: 该类需要嵌套在ScrollView 中使用，并且是横向布局
+ */
+public class ScrollHorizontalRecyclerView extends RecyclerView {
+
+    public ScrollHorizontalRecyclerView(Context context) {
+        super(context);
+    }
+
+    public ScrollHorizontalRecyclerView(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public ScrollHorizontalRecyclerView(Context context, @Nullable AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+    }
+
+    private float lastX, lastY;
+
+    @Override
+    public boolean onInterceptTouchEvent(MotionEvent e) {
+
+        boolean intercept = super.onInterceptTouchEvent(e);
+
+        switch (e.getAction()) {
+
+            case MotionEvent.ACTION_DOWN:
+                lastX = e.getX();
+                lastY = e.getY();
+                break;
+            case MotionEvent.ACTION_MOVE:
+                // 只要横向大于竖向，就拦截掉事件。
+                // 部分机型点击事件（slopx==slopy==0），会触发MOVE事件。
+                // 所以要加判断(slopX > 0 || sloy > 0)
+                float slopX = Math.abs(e.getX() - lastX);
+                float slopY = Math.abs(e.getY() - lastY);
+                //  Log.log("slopX=" + slopX + ", slopY="  + slopY);
+                if((slopX > 0 || slopY > 0) && slopX >= slopY){
+                    requestDisallowInterceptTouchEvent(true);
+                    intercept = true;
+                }
+                break;
+            case MotionEvent.ACTION_UP:
+                intercept = false;
+                break;
+        }
+        // Log.log("intercept"+e.getAction()+"=" + intercept);
+        return intercept;
+    }
+
+}
+
